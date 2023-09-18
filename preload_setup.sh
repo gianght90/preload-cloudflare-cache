78b3ab3715e637e625060b6a3211a84697d6de77
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-09-18 21:01:14 +0700

    Update preload_setup.sh

--- preload_setup.sh
+++ preload_setup.sh
@@ -1,6 +1,6 @@
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/monitor-service-status/main/monitor_service_restart.sh -O /usr/local/bin/monitor_service_restart.sh
-chmod +x /usr/local/bin/monitor_service_restart.sh
-nohup /usr/local/bin/monitor_service_restart.sh >> ./out 2>&1 <&- &
-crontab -l > monitor_service_restart
-echo "@reboot nohup /usr/local/bin/monitor_service_restart.sh >> ./out 2>&1 <&- &" >> monitor_service_restart
-crontab monitor_service_restart
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/preload-cloudflare-cache/main/precache.sh -O /usr/local/bin/precache_cloudflare.sh
+chmod +x /usr/local/bin/precache_cloudflare.sh
+nohup /usr/local/bin/precache_cloudflare.sh >> ./out 2>&1 <&- &
+crontab -l > precache_cloudflare
+echo "@reboot nohup /usr/local/bin/monitor_service_restart.sh >> ./out 2>&1 <&- &" >> precache_cloudflare
+crontab precache_cloudflare
